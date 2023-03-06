56a51c847ad69d881f1d9ca125fb1390849e5631
naser421 <naser421@gmail.com>
2023-03-07 02:00:08 +0300

    additional fixes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 
-// add apollo server
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
@@ -10,7 +9,6 @@ const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// add apollo middleware
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -27,7 +25,6 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
