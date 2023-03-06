8e15d250c450094b29e0b765773c5be317ba8afa
naser421 <naser421@gmail.com>
2023-03-07 02:06:21 +0300

    db update

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useFindAndModify: false,
 });
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection; 
